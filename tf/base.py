5d968f9e3d620fa55122ad6334b68b4f38644620
surchs <sebastian.urchs@gmail.com>
2022-05-01 19:50:44 -0400

    pr commit

--- tf/base.py
+++ tf/base.py
@@ -1,2 +1,9 @@
 def friendlyfunc(value, addition):
     return value + addition
+
+
+def other_func(word, addition):
+    if isinstance(word, str):
+        return ' '.join((word, addition))
+    else:
+        return friendlyfunc(word, addition)
--- tf/tests/test_tf.py
+++ tf/tests/test_tf.py
@@ -4,3 +4,9 @@ def test_tf():
     val1, val2 = 1, 2
     result = base.friendlyfunc(val1, val2)
     assert result == 3
+
+
+def test_otherfunc():
+    word1, word2 = 'hello', 'world'
+    result = base.other_func(word1, word2)
+    assert result == 'hello world'
